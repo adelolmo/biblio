e03e6a6bede70ea6be76f7b89e14cc66e2adfa9b
adelolmo <andoni_del.olmo_elizari@smeet.com>
2015-02-02 16:42:49 +0100

    fix server war.

---
+++ biblio-server-war/src/main/java/org/ado/biblio/config/AppInitializer.java
@@ -0,0 +1,27 @@
+package org.ado.biblio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+public class AppInitializer  implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = container.addServlet(
+                "dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+}
