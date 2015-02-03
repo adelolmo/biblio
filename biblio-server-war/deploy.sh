dcc88ec4a93008e321d6d75ca27de03b88e2b68d
andoni <andoni.delolmo@gmail.com>
2015-02-03 21:29:17 +0100

    server-war - add version.txt to server.

--- biblio-server-war/deploy.sh
+++ biblio-server-war/deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "======================"
 echo "Deploying to OpenShift"
+echo "======================"
+echo
+
+echo "> generate version.txt"
+ls target/biblio-server-war-*.war > version.txt
+scp version.txt 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
+
+echo "> copy artifact"
 mkdir -p /tmp/biblio-server-war
 cp target/biblio-server-war*.war /tmp/biblio-server-war/ROOT.war
 scp /tmp/biblio-server-war/ROOT.war 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
