584983787e110e3e51f78a75175be6422b8c6270
andoni <andoni.delolmo@gmail.com>
2015-03-11 19:29:26 +0100

    server. fix version.txt

--- biblio-server-war/deploy.sh
+++ biblio-server-war/deploy.sh
@@ -6,11 +6,12 @@ echo "======================"
 echo
 
 echo "> generate version.txt"
-ls target/biblio-server-war-*.war > version.txt
-scp version.txt 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
-rm version.txt
+ls target/biblio-server-war-*.war|grep -oE "([0-9]\.[0-9]+)(-SNAPSHOT)?"> /tmp/version.txt
+scp /tmp/version.txt 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
+rm /tmp/version.txt
+echo "Version: `cat /tmp/version.txt`"
 
-echo "> copy artifact"
+echo "> deploying artifact"
 mkdir -p /tmp/biblio-server-war
 cp target/biblio-server-war*.war /tmp/biblio-server-war/ROOT.war
 scp /tmp/biblio-server-war/ROOT.war 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
