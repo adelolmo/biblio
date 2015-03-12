de5d2fa51baf1a964be06b236366db5c32aff2b6
andoni <andoni.delolmo@gmail.com>
2015-03-12 06:50:09 +0100

    server. fix version.txt

--- biblio-server-war/deploy.sh
+++ biblio-server-war/deploy.sh
@@ -8,8 +8,8 @@ echo
 echo "> generate version.txt"
 ls target/biblio-server-war-*.war|grep -oE "([0-9]\.[0-9]+)(-SNAPSHOT)?"> /tmp/version.txt
 scp /tmp/version.txt 54d06aed4382ecb74b00008b@biblio-adoorg.rhcloud.com:/var/lib/openshift/54d06aed4382ecb74b00008b/app-root/dependencies/jbossews/webapps/
-rm /tmp/version.txt
 echo "Version: `cat /tmp/version.txt`"
+rm /tmp/version.txt
 
 echo "> deploying artifact"
 mkdir -p /tmp/biblio-server-war
