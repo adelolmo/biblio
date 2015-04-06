d256ce31bc0f727b5b289a18aa9de61290a9736b
andoni <andoni.delolmo@gmail.com>
2015-04-06 19:47:46 +0200

    rolback release.

--- release.sh
+++ release.sh
@@ -10,13 +10,13 @@ usage(){
 }
 
 run(){
-	$MVN versions:set -DnewVersion="$RELEASE_VERSION"
-	git commit -a -m "prepare to release v.$RELEASE_VERSION"
-	git push
-	$MVN clean install -Prelease
-	$MVN versions:set -DnewVersion="$DEVELOPMENT_VERSION-SNAPSHOT"
-	git commit -a -m "prepare to develop v.$DEVELOPMENT_VERSION-SNAPSHOT"
-	git push
+	$MVN versions:set -DnewVersion="$RELEASE_VERSION" &&
+	git commit -a -m "prepare to release v.$RELEASE_VERSION" &&
+	git push &&
+	$MVN clean install -Prelease &&
+	$MVN versions:set -DnewVersion="$DEVELOPMENT_VERSION-SNAPSHOT" &&
+	git commit -a -m "prepare to develop v.$DEVELOPMENT_VERSION-SNAPSHOT" &&
+	git push &&
 	find -name "*.versionsBackup"| xargs -I file rm file
 }
 
